f4edea2f5cf6859d2115b9514e259a2ad0b92b56
Gedearya <igd.ad.pratama@gmail.com>
2025-12-10 21:07:28 +0700

    Fix age variable value in type annotation example

--- script.ts
+++ script.ts
@@ -1,4 +1,17 @@
+// =============================
+//  1. BASIC TYPE ANNOTATION
+// =============================
+
 // Type primitive
 let username: string = "Gedearya";
-let age: number = 27;
+let age: number = 24;
 let isDev: boolean = true;
+
+// Tipe array
+let skills: string[] = ["HTML", "CSS", "JavaScript", "TypeScript"];
+
+// Union type (bisa lebih dari satu tipe)
+let id: number | string = "USR-001";
+
+console.log("User:", username);
+console.log("Skills:", skills);
